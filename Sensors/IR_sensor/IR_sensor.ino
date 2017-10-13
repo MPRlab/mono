e078e9dcb7aaf03f79ba12211ef030238eb161ed
scottbarton <sdbarton@humanities-nb07-2.local>
2017-10-13 12:34:37 -0400

    sync

--- Sensors/IR_sensor/IR_sensor.ino
+++ Sensors/IR_sensor/IR_sensor.ino
@@ -12,6 +12,7 @@ void setup() {
 }
 
 void loop() {
+<<<<<<< Updated upstream
 <<<<<<< HEAD
   x = analogRead(1);
   Serial.println(x);  
@@ -19,4 +20,10 @@ void loop() {
   distance = analogRead(1);
   Serial.println(distance);
 >>>>>>> origin/master
+=======
+  for (int i=0; i<100; i++) {
+    Serial.println(i);
+    delay(1000);
+  }
+>>>>>>> Stashed changes
 }
