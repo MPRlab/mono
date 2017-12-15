525ac36d6ed565bcb3b478b03583afc7aa301c68
Chris Bove <bove.3123@gmail.com>
2017-12-15 10:26:56 -0500

    remove print statements in driver

--- MultiSolenoidDriver_Code_V1_WithStepper/MultiSolenoidDriver_Code_V1/StepperController.h
+++ MultiSolenoidDriver_Code_V1_WithStepper/MultiSolenoidDriver_Code_V1/StepperController.h
@@ -70,18 +70,15 @@ class StepperController{
 					
 					_step(FORWARD);
 					_status->stepperStepsLeft.set(stepsLeft - 1);
-         Serial.print("Forward StepsLeft: "); Serial.print(stepsLeft); Serial.print(" current_step: "); Serial.print(_currentStep);
-         Serial.print(" millis: "); Serial.println(millis());
 				} else if (stepsLeft < 0) {
           wakeUp();
 					_timeOfLastMovement = millis();
           
 					_step(REVERSE);
 					_status->stepperStepsLeft.set(stepsLeft + 1);
-				  Serial.println("Reverse");
 				} else if ((millis() - _timeOfLastMovement) > TIME_UNTIL_SLEEP) { // else if stepsLeft is zero, sleep the stepper.
           goToSleep();
-				} else { /*Serial.println("not time for sleep");*/}
+				}
 				// Save time of this step
 				_timeOfLastStep = millis();
 
@@ -202,7 +199,6 @@ class StepperController{
    void goToSleep(){
       if (!sleeping) {
         sleeping = true;
-        Serial.println("Sleepy Time!");
         digitalWrite(_nsleep, LOW);
       }
    }
